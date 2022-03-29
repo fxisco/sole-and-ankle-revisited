c478daad8df20ef25dc33ccec8a7f21fb7152d59
Francisco Payés <payesfrancisco@gmail.com>
2022-03-29 11:52:04 +0200

    Exercise 5: Fluid desktop navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -61,9 +61,9 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
   border-top: 4px solid ${COLORS.gray[900]};
+  overflow: auto;
 
   @media ${QUERIES.tabletAndSmaller} {
     justify-content: space-between;
@@ -95,7 +95,7 @@ const ShoppingBagButton = styled(UnstyledButton)`
 
 const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 9.2vw - 4.5rem, 3.5rem);
   margin: 0px 48px;
 
   @media ${QUERIES.tabletAndSmaller} {
